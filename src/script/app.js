053fe95e38ecd9a05810405f5d08af54e60b6b49
WebCONuz <muxammadi0799@gmail.com>
2023-02-14 11:27:35 +0500

    First commit

---
+++ src/script/app.js
@@ -0,0 +1,184 @@
+const app = Vue.createApp({
+  // Ma'lumotlar saqlanadi
+  data() {
+    return {
+      message: "Hello Vue framework",
+      toggle: true,
+      navLinks: [
+        { id: 1, title: "Dashboard", href: "#" },
+        { id: 2, title: "Pages", href: "#" },
+        { id: 3, title: "Users", href: "#" },
+        { id: 4, title: "Lists", href: "#" },
+        { id: 5, title: "Info", href: "#" },
+        { id: 6, title: "Services", href: "#" },
+      ],
+      x: 0,
+      y: 0,
+      tasks: [],
+      taskName: "",
+      deadLine: "",
+      users: [
+        {
+          id: 1,
+          email: "george.bluth@reqres.in",
+          first_name: "George",
+          last_name: "Bluth",
+          avatar: "https://reqres.in/img/faces/1-image.jpg",
+          isOnline: true,
+        },
+        {
+          id: 2,
+          email: "janet.weaver@reqres.in",
+          first_name: "Janet",
+          last_name: "Weaver",
+          avatar: "https://reqres.in/img/faces/2-image.jpg",
+          isOnline: true,
+        },
+        {
+          id: 3,
+          email: "emma.wong@reqres.in",
+          first_name: "Emma",
+          last_name: "Wong",
+          avatar: "https://reqres.in/img/faces/3-image.jpg",
+          isOnline: false,
+        },
+        {
+          id: 4,
+          email: "eve.holt@reqres.in",
+          first_name: "Eve",
+          last_name: "Holt",
+          avatar: "https://reqres.in/img/faces/4-image.jpg",
+          isOnline: false,
+        },
+        {
+          id: 5,
+          email: "charles.morris@reqres.in",
+          first_name: "Charles",
+          last_name: "Morris",
+          avatar: "https://reqres.in/img/faces/5-image.jpg",
+          isOnline: false,
+        },
+        {
+          id: 6,
+          email: "tracey.ramos@reqres.in",
+          first_name: "Tracey",
+          last_name: "Ramos",
+          avatar: "https://reqres.in/img/faces/6-image.jpg",
+          isOnline: true,
+        },
+        {
+          id: 7,
+          email: "michael.lawson@reqres.in",
+          first_name: "Michael",
+          last_name: "Lawson",
+          avatar: "https://reqres.in/img/faces/7-image.jpg",
+          isOnline: false,
+        },
+        {
+          id: 8,
+          email: "lindsay.ferguson@reqres.in",
+          first_name: "Lindsay",
+          last_name: "Ferguson",
+          avatar: "https://reqres.in/img/faces/8-image.jpg",
+          isOnline: false,
+        },
+        {
+          id: 9,
+          email: "tobias.funke@reqres.in",
+          first_name: "Tobias",
+          last_name: "Funke",
+          avatar: "https://reqres.in/img/faces/9-image.jpg",
+          isOnline: false,
+        },
+        {
+          id: 10,
+          email: "byron.fields@reqres.in",
+          first_name: "Byron",
+          last_name: "Fields",
+          avatar: "https://reqres.in/img/faces/10-image.jpg",
+          isOnline: true,
+        },
+        {
+          id: 11,
+          email: "george.edwards@reqres.in",
+          first_name: "George",
+          last_name: "Edwards",
+          avatar: "https://reqres.in/img/faces/11-image.jpg",
+          isOnline: false,
+        },
+        {
+          id: 12,
+          email: "rachel.howell@reqres.in",
+          first_name: "Rachel",
+          last_name: "Howell",
+          avatar: "https://reqres.in/img/faces/12-image.jpg",
+          isOnline: false,
+        },
+      ],
+      baseUrl: "https://jsonplaceholder.typicode.com/posts",
+      postList: [],
+      loading: false,
+    };
+  },
+
+  // Ham ma'lumotlar, ham metodlar saqlanadi
+  // Metodlar va ularning qaytargan qiymatini saqlaydi. Bu yerda filterOnline => [online users]
+  computed: {
+    filterOnline() {
+      return this.users.filter((item) => item.isOnline);
+    },
+    filterOffline() {
+      return this.users.filter((item) => !item.isOnline);
+    },
+  },
+
+  // Ma'lumotlarni o'zgartiruvchi metodlar saqlanadi
+  methods: {
+    setToggle() {
+      this.toggle = !this.toggle;
+      // this.$refs.toggleMenu.classList.toggle("swipe");
+    },
+    mouseMove(e) {
+      this.x = e.clientX;
+      this.y = e.clientY;
+    },
+    addNewTask() {
+      const newTask = {
+        id: Date.now(),
+        title: this.taskName,
+        date: this.deadLine,
+        isDone: false,
+      };
+
+      if (
+        newTask.title.trim().length === 0 ||
+        newTask.date.trim().length === 0
+      ) {
+        console.log("Please, fill in the task title and date");
+      } else {
+        this.tasks.push(newTask);
+        this.taskName = "";
+        this.deadLine = "";
+      }
+    },
+  },
+
+  // Yaratilish
+  mounted() {
+    console.log("Hello vue.js");
+    const getPosts = async () => {
+      try {
+        const posts = await fetch(this.baseUrl);
+        const result = await posts.json();
+        this.postList = result;
+        this.loading = true;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPosts();
+  },
+});
+
+// HTMLga yozish
+app.mount("#root");
